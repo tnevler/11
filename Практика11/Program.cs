22e6a5322b32fe9835a94f2115b55b62ee3f3040
user <user@DESKTOP-0L3M84E>
2019-06-08 01:00:22 +0500

    Добавьте файлы проекта.

---
+++ Практика11/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика11
+{
+    public class Program
+    {
+        static public char[] Coding(string a)
+        {
+            char[] b = new char[a.Length];
+            b[0] = a[0];
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] == a[i - 1])
+                {
+                    if (a[0] == '0' || a[0] == '1') b[i] = '1';
+                    else b[i] = '-';
+                }
+                else
+                {
+                    if (a[0] == '0' || a[0] == '1') b[i] = '0';
+                    else b[i] = '.';
+                }
+            }
+            return b;
+        }
+
+        static void Main(string[] args)
+        {
+            string a = null;
+            bool ok = false;
+            do
+            {
+                Console.WriteLine("Введите строку, состоящую из 0 и 1 (или '.' и '-')");
+                a = Console.ReadLine();
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[0] == '0' || a[0] == '1')
+                        if (a[i] == '0' || a[i] == '1') ok = true;
+                        else
+                        {
+                            ok = false;
+                            break;
+                        }
+                    if (a[0] == '.' || a[0] == '-')
+                        if (a[i] == '.' || a[i] == '-') ok = true;
+                        else
+                        {
+                            ok = false;
+                            break;
+                        }
+                }
+            } while (!ok);
+
+            char[] b = Coding(a);
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Тестирование11/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Практика11;
+
+namespace Тестирование11
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string a = "01110001";
+            string result = "00110110";
+            char[] b = Program.Coding(a);
+            string b1 = string.Concat(b);
+            Assert.AreEqual(result, b1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string a = ".---...-";
+            string result = "..--.--.";
+            char[] b = Program.Coding(a);
+            string b1 = string.Concat(b);
+            Assert.AreEqual(result, b1);
+        }
+    }
+}
